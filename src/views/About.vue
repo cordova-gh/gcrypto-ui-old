8966d18137a1635228cd755820e2006ae70a312d
geancarlocordova <geancarlo.cordova@kireygroup.com>
2020-08-16 10:51:18 +0200

    first commit

---
+++ src/views/About.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <button type="button" class="btn btn-primary">
+      Profile
+      <span class="badge badge-light">9</span>
+      <span class="sr-only">unread messages</span>
+    </button>
+  </div>
+</template>
