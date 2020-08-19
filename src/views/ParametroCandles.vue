4cf975f6780fd452fadaa23512a98821e1cff9b5
Gaia Gulinelli <ibryan2205@gmail.com>
2020-08-19 20:57:05 +0200

    risolto scambio tra pairs e candles

--- src/views/ParametroCandles.vue
+++ src/views/ParametroCandles.vue
@@ -1,40 +1,56 @@
 <template>
   <div class="about">
-    <h1>This is a Candles page</h1>
+    <h1>This is a Parametro Pairs page</h1>
       <table class="table">
         <thead>
           <tr>
             <th>ID</th>
-            <th>First</th>
-            <th>Second</th>
+            <th>Intervallo</th>
+            <th>Alert Low</th>
+            <th>Alert Medium</th>
+            <th>Alert High</th>
+            <th>Percentual</th>
+            <th>BB count</th>
           </tr>
         </thead>
         <tbody>
           <tr v-for="(row, index) of list" :key="index">
-            <td>{{ row.id}}</td>
-            <td>{{ row.first_cv}}</td>
-            <td>{{ row.second_cv}}</td>
+            <td>{{row.id_parametro_pair_cv}}</td>
+            <td>{{row.intervallo }}</td>
+            <td>{{row.diff_alert_low}}</td>
+            <td>{{row.diff_alert_medium}}</td>
+            <td>{{row.diff_alert_high}}</td>
+            <td>{{row.perc_alert}}</td>
+            <td>{{row.bearish_bullish_count}}</td>
           </tr>
         </tbody>
       </table>
   </div>
+
   <form>
     <div class="form-group">
-      Id:
+      Id Parametro:
       <select name="id_parametro_pair_cv">
         <option value="1">BTC/USDT</option>
-      </select>      
-      First:
-      <input type="text" />
-      Second:
-      <input type="text" />
+      </select>
+      Alert Low:
+      <input type="text"/>
+      Alert Medium:
+      <input type="text"/>
+      Alert High:
+      <input type="text"/>
+      Percentual:
+      <input type="checkbox" />
+      BB Count:
+      <input type="checkbox" />
 
       <input type="submit" value="Invio" />
     </div>
   </form>
 </template>
+
 <script>
-import axios from "axios";
+import Axios from "axios";
 export default {
   data() {
     return {
@@ -42,9 +58,7 @@ export default {
     };
   },
   created() {
-    axios
-      .get("https://gcrypto.herokuapp.com/parametro-candle-cvs")
-
+    Axios.get("https://gcrypto.herokuapp.com/parametro-candle-cvs")
       .then((response) => {
         this.list = response.data;
         console.log(response);
@@ -57,3 +71,5 @@ export default {
   },
 };
 </script>
+
+  
