12e3819811597e5e55a57932953806c4af64c073
geancarlocordova <geancarlo.cordova@kireygroup.com>
2020-08-19 22:03:42 +0200

    add method post for entity parametro pair

--- README.md
+++ README.md
@@ -32,3 +32,39 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+### linee guida
+-----------------
+CAMEL CASE
+
+Raw: user login count
+Camel Case: userLoginCount
+
+PASCAL CASE
+
+Raw: user login count
+Pascal Case: UserLoginCount
+
+
+SNAKE CASE
+
+Raw: user login count
+Snake Case: user_login_count
+Snake Case (All Caps): USER_LOGIN_COUNT
+
+KEBAB CASE
+
+Raw: user login count
+Kebab Case: user-login-count
+
+
+-----------------
+
+
+per i componenti vue utilizzeremo  PASCAL CASE
+per i routes utilizzeremo il KEBAB CASE
+per i campi del database si usa lo snake case
+per i campi di ritorno tramite chiamata http camel case ( in questo momento stiamo utilizzando lo snake)
+per gli endpoint http utilizzaremo kebab case 
+
+
--- src/views/ParametroPairs.vue
+++ src/views/ParametroPairs.vue
@@ -1,37 +1,40 @@
 <template>
-  <div class="about">
-    <h1>This is a Candles page</h1>
-      <table class="table">
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>First</th>
-            <th>Second</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="(row, index) of list" :key="index">
-            <td>{{ row.id}}</td>
-            <td>{{ row.first_cv}}</td>
-            <td>{{ row.second_cv}}</td>
-          </tr>
-        </tbody>
-      </table>
-  </div>
-  <form>
-    <div class="form-group">
-      Id:
-      <select name="id_parametro_pair_cv">
-        <option value="1">BTC/USDT</option>
-      </select>      
-      First:
-      <input type="text" />
-      Second:
-      <input type="text" />
+  <div class="about" v-if="!visualizzaForm">
+    <h1>Parametri Pair</h1>
+     <input type="button" class="btn btn-primary" value="Inserisci" @click="prepareInsert">
+    <table class="table">
+      <thead>
+        <tr>
+         
+          <th>First</th>
+          <th>Second</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(row, index) of list" :key="index">
 
-      <input type="submit" value="Invio" />
-    </div>
-  </form>
+          <td>{{ row.first_cv}}</td>
+          <td>{{ row.second_cv}}</td>
+        </tr>
+      </tbody>
+    </table>
+   
+  </div>
+  <div class="container" v-if="visualizzaForm"> 
+    <h2>Crea</h2>
+    <form @submit.prevent="saveEntity">
+   
+      <div class="form-group">
+        <label for="first">First:</label>
+        <input type="text" class="form-control" v-model="parametroPair.first_cv" />
+      </div>
+      <div class="form-group">
+        <label for="second">Second:</label>
+        <input type="text" class="form-control" v-model="parametroPair.second_cv" />
+      </div>
+      <button type="submit" class="btn btn-primary">Crea</button>
+    </form>
+  </div>
 </template>
 <script>
 import axios from "axios";
@@ -39,12 +42,14 @@ export default {
   data() {
     return {
       list: [],
+      parametroPair: {},
+      visualizzaForm: false
     };
   },
   created() {
+    
     axios
       .get("https://gcrypto.herokuapp.com/parametro-pair-cvs")
-
       .then((response) => {
         this.list = response.data;
         console.log(response);
@@ -54,6 +59,28 @@ export default {
         this.errored = true;
       })
       .finally(() => (this.loading = false));
+  },
+  methods: {
+    saveEntity() {
+      console.log("entità", this.entity);
+      return axios
+        .post("http://localhost:5000/parametro-pair-cvs", this.parametroPair, {
+          headers: {
+            Accept: "application/json",
+            "Content-type": "application/json",
+          },
+        })
+        .then(function (response) {
+          return response.data;
+        })
+        .catch(function (error) {
+          console.log("errore ", error);
+          return "An error occured.." + error;
+        });
+    },
+    prepareInsert(){
+      this.visualizzaForm= true;
+    }
   },
 };
 </script>
